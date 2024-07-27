f61f6f2f4fb8547fc93e1282267ae0067b94c5e2
tomaz1502 <tomgm1502@gmail.com>
2024-07-28 01:52:02 +0200

    Progress on merge sort proof

--- Runtime/MergeSort/MergeSort.lean
+++ Runtime/MergeSort/MergeSort.lean
@@ -1,12 +1,13 @@
 import Runtime.MergeSort.LogLemmas
 import Runtime.MergeSort.Merge
+import Runtime.MergeSort.Split
 
 universe u
 
 variable {α : Type} (r : α → α → Prop) [DecidableRel r]
 local infixl:50 " ≼ " => r
 
-def mergeSort : List α → (List α × Nat)
+@[simp] def mergeSort : List α → (List α × Nat)
   | [] => ([], 0)
   | [a] => ([a], 0)
   | a :: b :: l => by
@@ -30,17 +31,17 @@ theorem mergeSort_cons_cons {a b} {l l₁ l₂ : List α}
        rw [← h₁, ← h₂]
 
 theorem mergeSort_equivalence : ∀ (l : List α), (mergeSort r l).fst = List.mergeSort r l
-| []          => by simp [mergeSort]
-| [a]         => by simp [mergeSort]
-| a :: b :: l => by
-    have : (l.split.1).length < l.length + 1 := Nat.lt_add_one_of_le (List.length_split_fst_le l)
-    have : (l.split.2).length < l.length + 1 := Nat.lt_add_one_of_le (List.length_split_snd_le l)
-    rw [List.mergeSort_cons_cons r (Prod.ext rfl rfl)]
-    rw [mergeSort_cons_cons r (Prod.ext rfl rfl)]
-    rw [merge_equivalence]
-    rw [mergeSort_equivalence (a :: l.split.1)]
-    rw [mergeSort_equivalence (b :: l.split.2)]
-  termination_by l => List.length l
+  | []          => by simp [mergeSort]
+  | [a]         => by simp [mergeSort]
+  | a :: b :: l => by
+      have : (l.split.1).length < l.length + 1 := Nat.lt_add_one_of_le (List.length_split_fst_le l)
+      have : (l.split.2).length < l.length + 1 := Nat.lt_add_one_of_le (List.length_split_snd_le l)
+      rw [List.mergeSort_cons_cons r (Prod.ext rfl rfl)]
+      rw [mergeSort_cons_cons r (Prod.ext rfl rfl)]
+      rw [merge_equivalence]
+      rw [mergeSort_equivalence (a :: l.split.1)]
+      rw [mergeSort_equivalence (b :: l.split.2)]
+    termination_by l => List.length l
 
 theorem mergeSort_cons_cons_snd {a b} {l l₁ l₂ : List α}
   (hs : List.split (a :: b :: l) = (l₁, l₂)) :
@@ -50,5 +51,48 @@ theorem mergeSort_cons_cons_snd {a b} {l l₁ l₂ : List α}
   simp at hs
   simp [mergeSort, hs]
 
-theorem merge_sort_complexity : ∀ l : List α,
-  (mergeSort r l).snd ≤ 8 * l.length * Nat.log 2 l.length := sorry
+theorem mergeSort_complexity : ∀ l : List α,
+    (mergeSort r l).snd ≤ 8 * l.length * Nat.log 2 l.length
+  | [] => by simp
+  | [a] => by simp
+  | a :: b :: l => by
+    cases e : List.split (a :: b :: l) with
+    | mk l₁ l₂ =>
+      rw [mergeSort_cons_cons_snd r e]
+      cases e1 : mergeSort r l₁ with
+      | mk ms1 n1 =>
+        cases e2 : mergeSort r l₂ with
+        | mk ms2 n2 =>
+          simp
+          have split_lt := @List.length_split_lt α a b l l₁ l₂ e
+          have := split_lt.1
+          have := split_lt.2
+
+          have ih1 := mergeSort_complexity l₁
+          have ih2 := mergeSort_complexity l₂
+          rw [e1] at ih1
+          simp at ih1
+          rw [e2] at ih2
+          simp at ih2
+
+          have :
+            n1 + n2 + (merge.loop (r · ·) ms1 ms2 []).2 ≤
+            (8 * l₁.length * Nat.log 2 l₁.length) + n2 + (merge.loop (r · ·) ms1 ms2 []).2 :=
+              by linarith
+
+          apply le_trans this
+          have :
+            (8 * l₁.length * Nat.log 2 l₁.length) +
+            n2 +
+            (merge.loop (r · ·) ms1 ms2 []).2
+            ≤
+            (8 * l₁.length * Nat.log 2 l₁.length) +
+            (8 * l₂.length * Nat.log 2 l₂.length) +
+            (merge.loop (r · ·) ms1 ms2 []).2 := by linarith
+          apply le_trans this
+          have ⟨l₁_small, l₂_small⟩ := split_halves_length e
+
+          admit
+
+    termination_by l => List.length l
+
--- Runtime/MergeSort/Split.lean
+++ Runtime/MergeSort/Split.lean
@@ -6,108 +6,73 @@ section timedSort
 
 universe u
 
-variable {α : Type u} [DecidableRel r]
+variable {α : Type u}
 
-/- @[simp] def split : List α → (List α × List α × Nat) -/
-/-   | []       => ([], [], 0) -/
-/-   | (h :: t) => let (l₁, l₂, n) := split t -/
-/-                 (h :: l₂, l₁, n + 1) -/
-
-/- theorem split_equivalence : ∀ (l : List α) , -/
-/-   (split l).fst = (List.split l).fst ∧ (split l).snd.fst = (List.split l).snd -/
-/-   | [] => by simp -/
-/-   | (h :: t) => by -/
-/-     simp -/
-/-     have ⟨ih_fst, ih_snd⟩ := split_equivalence t -/
-/-     exact And.intro ih_snd ih_fst -/
-
-/- theorem split_complexity : ∀ (l : List α) , (split l).snd.snd = l.length -/
-/- | [] => by simp -/
-/- | (h :: t) => by -/
-/-   simp -/
-/-   exact split_complexity t -/
-
-theorem length_split_lt {a b: α} {l l₁ l₂ : List α} {n : Nat}
-  (h : List.split (a::b::l) = (l₁, l₂)):
-    List.length l₁ < List.length (a::b::l) ∧
-    List.length l₂ < List.length (a::b::l) := by
-  have split_eq_full : l₁ = (a::b::l).split.fst ∧ l₂ = (a::b::l).split.snd := by
-    have l₂_n_id : (l₂, n) = (List.split (a :: b :: l)).snd :=
-      (congrArg Prod.snd h).congr_right.mpr rfl
-    have l₂_id : l₂ = (split (a :: b :: l)).snd.fst :=
-      (congrArg Prod.fst l₂_n_id).congr_right.mp rfl
-    have l₁_id : l₁ = (split (a :: b :: l)).fst :=
-      (congrArg Prod.fst h).congr_right.mpr rfl
-    have ⟨l₁_id', l₂_id'⟩ := split_equivalence (a :: b :: l)
-    apply And.intro
-    { rw [l₁_id'] at l₁_id; exact l₁_id }
-    rw [l₂_id'] at l₂_id
-    exact l₂_id
-  have ⟨l₁_id, l₂_id⟩ := split_eq_full
-  have reconstruct : List.split (a :: b :: l) = (l₁, l₂) := by
-    rw [l₁_id, l₂_id]
-  exact List.length_split_lt reconstruct
-
-theorem split_halves_length : ∀ {l l₁ l₂ : List α} {n : Nat},
-  split l = (l₁, l₂, n) →
+theorem split_halves_length' : ∀ {l l₁ l₂ : List α},
+  List.split l = (l₁, l₂) →
     2 * List.length l₁ ≤ List.length l + 1 ∧ 2 * List.length l₂ ≤ List.length l
-| []       => by
-  intros h
-  unfold split at h
-  simp at h
-  have ⟨h₁, ⟨h₂, _⟩⟩ := h
-  rw [← h₁, ← h₂]
-  simp
-| (a :: t) => by
-  intros h'
-  cases e: split t with
-  | mk t₁ t₂m =>
-    have ⟨t₂, m⟩ := t₂m
-    have split_id : split (a :: t) = (a :: t₂, t₁, m + 1) := by
-      unfold split
-      rw [e]
-    rw [split_id] at h'
-    injection h' with h₁' h''
-    injection h'' with h₂' h₃'
-    have ⟨ih₁, ih₂⟩ := split_halves_length e
-    apply And.intro
-    { rw [← h₁']
-      simp
-      exact @Nat.add_le_add (2 * List.length t₂) (List.length t) 2 2 ih₂ le_rfl
-    }
-    { rw [← h₂']
-      simp
-      exact ih₁
-    }
-
-theorem split_lengths : ∀ (l l₁ l₂ : List α) {n : Nat},
-  split l = (l₁, l₂, n) → l₁.length + l₂.length = l.length
-| []  => by
-  intros l₁ l₂ n
-  simp 
-  intros h₁ h₂ _
-  rw [← h₁, ← h₂]
-  simp
-| [a] => by
-  intros l₁ l₂ n
-  simp
-  intros h₁ h₂ _
-  rw [← h₁, ← h₂]
-  simp
-| (a :: b :: t) => by
-  intros l₁ l₂ n h
-  cases e : split t with
-  | mk l₁' l₂'m =>
-    have ⟨l₂', m⟩ := l₂'m
+  | []       => by
+    intros h
+    unfold List.split at h
     simp at h
-    rw [e] at h
-    unfold split at h
-    have ih := split_lengths t l₁' l₂' e
-    have ⟨h₁, ⟨h₂, _⟩⟩ := h
+    have ⟨h₁, h₂⟩ := h
     rw [← h₁, ← h₂]
     simp
-    show List.length l₁' + 1 + List.length l₂' + 1 = List.length t + 2
-    rw [add_comm, add_comm (List.length l₁'), add_assoc, ih]
-    ring_nf
+  | (a :: t) => by
+    intros h'
+    cases e: List.split t with
+    | mk t₁ t₂ =>
+      have split_id : List.split (a :: t) = (a :: t₂, t₁) := by
+        unfold List.split
+        rw [e]
+      rw [split_id] at h'
+      injection h' with h₁ h₂
+      have ⟨ih₁, ih₂⟩ := split_halves_length' e
+      apply And.intro
+      { rw [← h₁]
+        simp
+        exact @Nat.add_le_add (2 * List.length t₂) (List.length t) 2 2 ih₂ le_rfl
+      }
+      { rw [← h₂]
+        simp [ih₁]
+      }
+
+theorem split_halves_length : ∀ {l l₁ l₂ : List α},
+  List.split l = (l₁, l₂) →
+    List.length l₁ ≤ (List.length l + 1) / 2 ∧
+    List.length l₂ ≤ (List.length l) / 2 := by
+  intros l l₁ l₂ h
+  have ⟨pf₁, pf₂⟩ := split_halves_length' h
+  exact ⟨div_two (l.length + 1) l₁.length pf₁, div_two l.length l₂.length pf₂⟩
+
+/- theorem split_lengths : ∀ (l l₁ l₂ : List α) {n : Nat}, -/
+/-   split l = (l₁, l₂, n) → l₁.length + l₂.length = l.length -/
+/- | []  => by -/
+/-   intros l₁ l₂ n -/
+/-   simp -/ 
+/-   intros h₁ h₂ _ -/
+/-   rw [← h₁, ← h₂] -/
+/-   simp -/
+/- | [a] => by -/
+/-   intros l₁ l₂ n -/
+/-   simp -/
+/-   intros h₁ h₂ _ -/
+/-   rw [← h₁, ← h₂] -/
+/-   simp -/
+/- | (a :: b :: t) => by -/
+/-   intros l₁ l₂ n h -/
+/-   cases e : split t with -/
+/-   | mk l₁' l₂'m => -/
+/-     have ⟨l₂', m⟩ := l₂'m -/
+/-     simp at h -/
+/-     rw [e] at h -/
+/-     unfold split at h -/
+/-     have ih := split_lengths t l₁' l₂' e -/
+/-     have ⟨h₁, ⟨h₂, _⟩⟩ := h -/
+/-     rw [← h₁, ← h₂] -/
+/-     simp -/
+/-     show List.length l₁' + 1 + List.length l₂' + 1 = List.length t + 2 -/
+/-     rw [add_comm, add_comm (List.length l₁'), add_assoc, ih] -/
+/-     ring_nf -/
 
 end timedSort
